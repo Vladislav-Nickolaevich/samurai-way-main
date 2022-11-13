8471b034105f1924236bdd5bb6bbab1d08911452
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2022-11-14 00:43:29 +0300

    "Added components to post and divided into myPosts on new directories"

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import profClasses from './Profile.module.css'
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
@@ -15,16 +16,7 @@ const Profile = () => {
                 ava + descr
                 {/*<img src="https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg" alt=""/>*/}
             </div>
-            <div>
-                My posts
-                <div>New post</div>
-            </div>
-            <div className={profClasses.red}>
-                post 1
-            </div>
-            <div className={profClasses.red}>
-                post 2
-            </div>
+            <MyPosts/>
         </div>
 
     );
