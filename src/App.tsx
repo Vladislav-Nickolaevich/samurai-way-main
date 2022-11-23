dfe2e30bb86f048683ccf7e4f247edc99a62e5d0
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2022-11-23 07:40:44 +0300

    "Installed react router dom"

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
 import Dialogs from './components/Dialogs/Dialogs';
+import {Route, Routes} from 'react-router-dom';
+import Settings from "./components/Settings/Settings";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
 
 
 const App = () => {
@@ -13,8 +17,13 @@ const App = () => {
             <Header img={"https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg"}/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                {/*<Dialogs/>*/}
-                <Profile src={"https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"}/>
+                <Routes>
+                    <Route path="/dialogs" element={<Dialogs/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/news" element={<News/>}/>
+                    <Route path="/music" element={<Music/>}/>
+                    <Route path="/settings" element={<Settings/>}/>
+                </Routes>
             </div>
 
         </div>
