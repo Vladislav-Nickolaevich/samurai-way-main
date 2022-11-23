f6da42216b121f04b58b82ca215078cd946d6ec1
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2022-11-23 08:46:07 +0300

    Changed tag 'a' on tag 'NavLink', add style focus

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import navClasses from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 
-
+// style={({isActive}) => isActive? {color: 'red'}:{}}
+// classNameActive заменяется на то что сверху????
 // type NavbarType = 'Profile' | 'Messages' | 'News' | 'Mucis' | 'Settings'
 const Navbar = () => {
     return (
 
         <nav className={navClasses.nav}>
-            <div className={navClasses.item}>
-                <a href="/profile">Profile</a>
+            <div className={navClasses.item} >
+                <NavLink  to="/profile">Profile</NavLink>
             </div>
             <div className={navClasses.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs">Messages</NavLink>
             </div>
             <div className={navClasses.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news">News</NavLink>
             </div>
             <div className={navClasses.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music">Music</NavLink>
             </div>
 
             <div className={navClasses.item}>
-                <a href="/settings">Settings</a>
+                <NavLink to="/settings">Settings</NavLink>
             </div>
         </nav>
 
