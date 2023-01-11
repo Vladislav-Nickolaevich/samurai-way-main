ac8fdbc0c080c8159ec48e812a381df81da0488a
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-01-11 18:35:08 +0300

    Changed the version React Router Dom (6), map messages and users, created obj with DialogTypes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
 import Dialogs from './components/Dialogs/Dialogs';
-import {Route, Routes} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Settings from "./components/Settings/Settings";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
@@ -17,16 +17,13 @@ const App = () => {
                 <Header img={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ-DfbDa_apFhyZF4tENuL5UWFIb3lCtHK5f2AqBlFJKf7PDGqVOfrGS73OpU27s975mI&usqp=CAU"} />
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path="/dialogs" element={<Dialogs/>}/>
-                        <Route path="/profile" element={<Profile/>}/>
-                        <Route path="/news" element={<News/>}/>
-                        <Route path="/music" element={<Music/>}/>
-                        <Route path="/settings" element={<Settings/>}/>
-                    </Routes>
+                        <Route path="/dialogs" component={Dialogs}/>
+                        <Route path="/profile" component={Profile}/>
+                        <Route path="/news" component={News}/>
+                        <Route path="/music" component={Music}/>
+                        <Route path="/settings" component={Settings}/>
                 </div>
             </div>
-
     );
 }
 
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,29 +1,58 @@
 import React from 'react';
-import s from  './Dialogs.module.css'
+import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
+import {v1} from "uuid";
 
+type DialogType = {
+    id: string
+    userName: string
+    message: string
+}
+const obj: DialogType[] = [
+    {id: v1(),userName: 'Vlad', message: 'Hi'},
+    {id: v1(),userName: 'Alex', message: 'What is the best country for travelling?'},
+    {id: v1(),userName: 'Katya', message: 'I like country with sea'},
+    {id: v1(),userName: 'John', message: 'I want to see interesting places'},
+
+]
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
+            {/*
+            {/*    <div className={s.dialog}>*/}
+            {/*        <NavLink to='/dialogs/1'>Vlad</NavLink>*/}
+            {/*    </div>*/}
+            {/*    <div className={s.dialog}>*/}
+            {/*        <NavLink to='/dialogs/2'>Alex</NavLink>*/}
+            {/*    </div>*/}
+            {/*    <div className={s.dialog + ' ' + s.active}>*/}
+            {/*        <NavLink to='/dialogs/3'>Katya</NavLink>*/}
+            {/*    </div>*/}
+            {/*    <div className={s.dialog}>*/}
+            {/*        <NavLink to='/dialogs/4'>John</NavLink>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
             <div className={s.dialogsItems}>
-                <div className={s.dialog}>
-                    <NavLink to='vlad'>Vlad</NavLink>
+            { obj.map(el => {
+                return (
+                    <div key={el.id} className={s.dialog}>
+                        <NavLink to={'/dialogs/'+ el.id}>{el.userName}</NavLink>
                     </div>
-                <div className={s.dialog}>
-                    <NavLink to='alex'>Alex</NavLink>
-                </div>
-                <div className={s.dialog  + ' ' + s.active}>
-                     <NavLink to='katya'>Katya</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='john'>John</NavLink>
-                    </div>
+                )
+            })}
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>What is the best country for travelling?</div>
-                <div className={s.message}>I like country with sea</div>
-                <div className={s.message}>I want to see interesting places</div>
+                {obj.map(el => {
+                    return (
+                        <div className={s.message} key={el.id}>
+                            {el.message}
+                        </div>
+                    )
+                })}
+                {/*<div className={s.message}>Hi</div>*/}
+                {/*<div className={s.message}>What is the best country for travelling?</div>*/}
+                {/*<div className={s.message}>I like country with sea</div>*/}
+                {/*<div className={s.message}>I want to see interesting places</div>*/}
             </div>
         </div>
     );
