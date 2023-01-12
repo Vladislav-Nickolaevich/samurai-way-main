7332db0b359d314ee20f32ac909e49ee156b139b
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-01-12 08:23:04 +0300

    Created components DialogUsers and DialogMessage, added css

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -9,51 +9,43 @@ type DialogType = {
     message: string
 }
 const obj: DialogType[] = [
-    {id: v1(),userName: 'Vlad', message: 'Hi'},
-    {id: v1(),userName: 'Alex', message: 'What is the best country for travelling?'},
-    {id: v1(),userName: 'Katya', message: 'I like country with sea'},
-    {id: v1(),userName: 'John', message: 'I want to see interesting places'},
-
+    {id: v1(), userName: 'Vlad', message: 'Hi'},
+    {id: v1(), userName: 'Alex', message: 'What is the best country for travelling?'},
+    {id: v1(), userName: 'Katya', message: 'I like country with sea'},
+    {id: v1(), userName: 'John', message: 'I want to see interesting places'},
 ]
-const Dialogs = () => {
+
+const DialogUsers = () => {
     return (
-        <div className={s.dialogs}>
-            {/*
-            {/*    <div className={s.dialog}>*/}
-            {/*        <NavLink to='/dialogs/1'>Vlad</NavLink>*/}
-            {/*    </div>*/}
-            {/*    <div className={s.dialog}>*/}
-            {/*        <NavLink to='/dialogs/2'>Alex</NavLink>*/}
-            {/*    </div>*/}
-            {/*    <div className={s.dialog + ' ' + s.active}>*/}
-            {/*        <NavLink to='/dialogs/3'>Katya</NavLink>*/}
-            {/*    </div>*/}
-            {/*    <div className={s.dialog}>*/}
-            {/*        <NavLink to='/dialogs/4'>John</NavLink>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-            <div className={s.dialogsItems}>
-            { obj.map(el => {
+        <div className={s.dialogsItems}>
+            {obj.map(el => {
                 return (
                     <div key={el.id} className={s.dialog}>
-                        <NavLink to={'/dialogs/'+ el.id}>{el.userName}</NavLink>
+                        <NavLink to={'/dialogs/' + el.id}>{el.userName}</NavLink>
                     </div>
                 )
             })}
-            </div>
-            <div className={s.messages}>
-                {obj.map(el => {
-                    return (
-                        <div className={s.message} key={el.id}>
-                            {el.message}
-                        </div>
-                    )
-                })}
-                {/*<div className={s.message}>Hi</div>*/}
-                {/*<div className={s.message}>What is the best country for travelling?</div>*/}
-                {/*<div className={s.message}>I like country with sea</div>*/}
-                {/*<div className={s.message}>I want to see interesting places</div>*/}
-            </div>
+        </div>
+    )
+}
+const DialogMessage = () => {
+    return (
+        <div className={s.messages}>
+            {obj.map(el => {
+                return (
+                    <div className={s.message} key={el.id}>
+                        {el.message}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+const Dialogs = () => {
+    return (
+        <div className={s.dialogs}>
+            <DialogUsers/>
+            <DialogMessage/>
         </div>
     );
 };
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import  './MyPosts.module.css'
+import s from './MyPosts.module.css'
 import {Post} from "./Posts/Post";
 
 const MyPosts = () => {
     return (
-        <>
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
             <div>
-                <div>My posts </div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
-            <Post likeCounts={10} mes={'poost 1'} photo={"https://i.pinimg.com/736x/f5/27/41/f52741fb62bf1d821948a49204406bdc.jpg"}/>
-            <Post likeCounts={15} mes={'poost 2'} photo={"https://shapka-youtube.ru/wp-content/uploads/2021/02/prikolnaya-avatarka-dlya-patsanov.jpg"}/>
-        </>
-
+            <div className={s.posts}>
+                <Post likeCounts={10} mes={'Hi'}
+                      photo={"https://i.pinimg.com/736x/f5/27/41/f52741fb62bf1d821948a49204406bdc.jpg"}/>
+                <Post likeCounts={15} mes={"It's my first post"}
+                      photo={"https://shapka-youtube.ru/wp-content/uploads/2021/02/prikolnaya-avatarka-dlya-patsanov.jpg"}/>
+            </div>
+        </div>
     );
 };
 
