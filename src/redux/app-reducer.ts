2df860c7ab704d9629a9c261c0333760ac6179f4
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-09-25 23:17:44 +0300

    Created appReducer with AC, TC

---
+++ src/redux/app-reducer.ts
@@ -0,0 +1,38 @@
+import {getAuthMeData} from "./auth-reducer";
+import {AnyAction} from "redux";
+import {ThunkDispatch} from "redux-thunk";
+import {AppRootStateType} from "./redux-store";
+
+
+
+const appReducer = (state: InitialStateType = initialState, action: InitializedSuccessACType): InitialStateType => {
+    switch (action.type) {
+        case INITIALIZED_SUCCESS:
+            return {
+                ...state,
+                initialized: true
+            }
+        default:
+            return state
+    }
+}
+type InitializedSuccessACType = ReturnType<typeof initializedSuccessAC>
+const initializedSuccessAC = () => ({type: INITIALIZED_SUCCESS} as const)
+
+export const initializeAppTC = () => {
+    return (dispatch: ThunkDispatch<AppRootStateType, any, AnyAction >) => {
+       let promise =  dispatch(getAuthMeData())
+        Promise.all([promise])
+            .then(() => dispatch(initializedSuccessAC()))
+    }
+}
+
+
+export default appReducer
+
+const INITIALIZED_SUCCESS = 'INITIALIZED-SUCCESS'
+
+export type InitialStateType = typeof initialState
+let initialState = {
+    initialized: false
+}
